699c5c07eb8004b0750385ac0363f88cd84a3073
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2022-07-21 09:34:48 -0400

    Restructured the way queries are done
    Added forward geocoding alongside reverse geocoding and set up input handling to deal with multiple input formats

--- javascript/breweries.js
+++ javascript/breweries.js
@@ -3,37 +3,46 @@ const form = document.querySelector("form");
 const brewSearch = document.getElementById("brewery-search");
 const brewResults = document.getElementById("breweries-resp");
 const locationIQ_token = "pk.1edb9e195fddf5bd03898c8052521649";
+let states_dictionary = null;
 let currentLocation = null;
 
 form.onsubmit = async (b) => {
   b.preventDefault();
+  let position = null;
 
   const inputValue = brewSearch.value;
+  brewSearch.value = "";
   if (!inputValue) return;
-  try {
-    const res = await fetch(
-      `https://api.openbrewerydb.org/breweries/search?query=${inputValue}`
+
+  let zipMatch = inputValue.match(/\b\d{5}\b/g);
+  console.log(zipMatch);
+
+  if (zipMatch) {
+    position = await geoCode("zip", zipMatch[0]).catch((e) =>
+      displayError("Could not find location")
     );
+  } else {
+    if (inputValue.split(",").length !== 2) {
+      displayError("Please enter a valid search");
+    } else {
+      position = await geoCode("city_state", inputValue.split(",")).catch((e) =>
+        displayError("Could not find location")
+      );
+    }
+  }
 
-    console.log(res);
-
-    if (res.status !== 200) throw new Error("Location Not Found");
-    const breweries = await res.json();
-    console.log(breweries);
-    displayBreweries(breweries);
-
-    // console.log(brewery)
-    // console.log(brewery[0].name)
-    // console.log(brewery[1])
-    // console.log(brewery[2])
-  } catch (err) {
-    brewResults.innerHTML = err.message;
-    brewSearch.value = "";
+  if (position) {
+    searchBreweriesByPosition();
   }
 };
 
 const displayBreweries = (breweries) => {
+  console.log(currentLocation);
+  brewSearch.value = "";
   let container = document.createElement("div");
+  let showingNear = document.createElement("h2");
+  showingNear.innerHTML = `Showing you results near ${currentLocation.address.city}, ${currentLocation.address.state}`;
+  container.appendChild(showingNear);
   breweries.forEach((brewery) => {
     const {
       name,
@@ -80,6 +89,10 @@ const displayBreweries = (breweries) => {
   brewResults.replaceChildren(container);
 };
 
+const displayError = (e) => {
+  brewResults.innerHTML = e;
+};
+
 const searchBreweriesByPosition = async (position) => {
   const { lat, lon } = currentLocation;
   const res = await fetch(
@@ -89,6 +102,63 @@ const searchBreweriesByPosition = async (position) => {
   let breweries = await res.json();
   console.log(breweries);
   displayBreweries(breweries);
+};
+
+const geoCode = async (type, query) => {
+  console.log(type, query);
+  return new Promise(async (resolve, reject) => {
+    let res = null;
+    let json = null;
+    try {
+      switch (type) {
+        case "zip":
+          res = await fetch(
+            `https://us1.locationiq.com/v1/search?key=${locationIQ_token}&country=us&postalcode=${query}&format=json`
+          );
+          console.log(res);
+          json = await res.json();
+          if (json.error) {
+            reject(json.error);
+          }
+          let postCodes = json.filter((el) => el.type === "postcode");
+          console.log(postCodes);
+          if (postCodes.length === 1) {
+            await reverseGeocode([postCodes[0].lat, postCodes[0].lon]);
+            resolve(true);
+          } else {
+            console.log("rejecting");
+            reject();
+          }
+          break;
+        case "city_state":
+          console.log("about to refine query: ", refineQuery);
+          let refined = refineQuery(query);
+          if (!refined) reject();
+          let [city, state] = refined;
+          console.log("test");
+          console.log(refined);
+          res = await fetch(
+            `https://us1.locationiq.com/v1/search?key=${locationIQ_token}&country=us&city=${city}&state=${state}&format=json`
+          );
+          console.log(res);
+          json = await res.json();
+          console.log(json);
+          if (json.error) {
+            reject(json.error);
+          }
+          if (json.length === 1) {
+            console.log(json[0]);
+            await reverseGeocode([json[0].lat, json[0].lon]);
+            resolve(true);
+          } else {
+            reject();
+          }
+          break;
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 
 const reverseGeocode = async ([latitude, longitude]) => {
@@ -104,7 +174,20 @@ const reverseGeocode = async ([latitude, longitude]) => {
   });
 };
 
+const refineQuery = (props) => {
+  let [city, state] = props.map((el) => el.trim().toLowerCase());
+  console.log(city, state);
+  console.log("refining: ", states_dictionary);
+  let found = states_dictionary.find(
+    (el) => el.name === state || el.abbreviation === state
+  );
+  console.log(!!found);
+  return found ? [city, found.name] : null;
+};
+
 document.body.onload = async (e) => {
+  let jsonRes = await fetch("javascript/states_dictionary.json");
+  states_dictionary = await jsonRes.json();
   if (navigator.geolocation) {
     console.log("we have geolocation");
     navigator.geolocation.getCurrentPosition(
