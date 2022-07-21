67d0e1baa3302f8288a92b747b89cc12753f69e7
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2022-07-20 23:28:20 -0400

    Added LocationIQ and Geolocation API to get breweries near user on page load

--- javascript/breweries.js
+++ javascript/breweries.js
@@ -2,6 +2,8 @@ const brewSection = document.getElementById("brewery-section");
 const form = document.querySelector("form");
 const brewSearch = document.getElementById("brewery-search");
 const brewResults = document.getElementById("breweries-resp");
+const locationIQ_token = "pk.1edb9e195fddf5bd03898c8052521649";
+let currentLocation = null;
 
 form.onsubmit = async (b) => {
   b.preventDefault();
@@ -18,7 +20,7 @@ form.onsubmit = async (b) => {
     if (res.status !== 200) throw new Error("Location Not Found");
     const breweries = await res.json();
     console.log(breweries);
-    listBreweries(breweries);
+    displayBreweries(breweries);
 
     // console.log(brewery)
     // console.log(brewery[0].name)
@@ -30,7 +32,7 @@ form.onsubmit = async (b) => {
   }
 };
 
-const listBreweries = (breweries) => {
+const displayBreweries = (breweries) => {
   let container = document.createElement("div");
   breweries.forEach((brewery) => {
     const {
@@ -76,4 +78,45 @@ const listBreweries = (breweries) => {
     container.appendChild(info);
   });
   brewResults.replaceChildren(container);
+};
+
+const searchBreweriesByPosition = async (position) => {
+  const { lat, lon } = currentLocation;
+  const res = await fetch(
+    `https://api.openbrewerydb.org/breweries?by_dist=${lat},${lon}`
+  ).catch((e) => displayError(e));
+  if (res.status !== 200) displayError("Could not find location");
+  let breweries = await res.json();
+  console.log(breweries);
+  displayBreweries(breweries);
+};
+
+const reverseGeocode = async ([latitude, longitude]) => {
+  return new Promise(async (resolve, reject) => {
+    const res = await fetch(
+      `https://us1.locationiq.com/v1/reverse?key=${locationIQ_token}&lat=${latitude}&lon=${longitude}&format=json`
+    );
+
+    let location = await res.json();
+    console.log(location);
+    currentLocation = location;
+    resolve(true);
+  });
+};
+
+document.body.onload = async (e) => {
+  if (navigator.geolocation) {
+    console.log("we have geolocation");
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        console.log(position);
+        await reverseGeocode([
+          position.coords.latitude,
+          position.coords.longitude,
+        ]),
+          searchBreweriesByPosition();
+      },
+      (error) => console.log(error)
+    );
+  }
 };
