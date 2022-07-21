83f7f06932afd89f5461d65363cdb238cd6edc4c
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2022-07-20 23:03:27 -0400

    Changed the way item list is rendered

--- javascript/breweries.js
+++ javascript/breweries.js
@@ -1,50 +1,79 @@
-const brewSection = document.getElementById('brewery-section')
-const form = document.querySelector('form')
-const brewSearch = document.getElementById('brewery-search')
-const brewResults = document.getElementById('breweries-resp')
+const brewSection = document.getElementById("brewery-section");
+const form = document.querySelector("form");
+const brewSearch = document.getElementById("brewery-search");
+const brewResults = document.getElementById("breweries-resp");
 
-form.onsubmit = async b => {
-    b.preventDefault()
+form.onsubmit = async (b) => {
+  b.preventDefault();
 
-    const inputValue = brewSearch.value
-    if (!inputValue) return
-    try{
-        const res = await fetch(`https://api.openbrewerydb.org/breweries/search?query=${inputValue}&per_page=3`)
+  const inputValue = brewSearch.value;
+  if (!inputValue) return;
+  try {
+    const res = await fetch(
+      `https://api.openbrewerydb.org/breweries/search?query=${inputValue}`
+    );
 
-        console.log(res)
+    console.log(res);
 
-        if (res.status !== 200) throw new Error('Location Not Found')
-        const brewery = await res.json()
-        listBreweries(brewery)
+    if (res.status !== 200) throw new Error("Location Not Found");
+    const breweries = await res.json();
+    console.log(breweries);
+    listBreweries(breweries);
 
-        console.log(brewery)
-        console.log(brewery[0].name)
-        console.log(brewery[1])
-        console.log(brewery[2])
+    // console.log(brewery)
+    // console.log(brewery[0].name)
+    // console.log(brewery[1])
+    // console.log(brewery[2])
+  } catch (err) {
+    brewResults.innerHTML = err.message;
+    brewSearch.value = "";
+  }
+};
+
+const listBreweries = (breweries) => {
+  let container = document.createElement("div");
+  breweries.forEach((brewery) => {
+    const {
+      name,
+      brewery_type,
+      street,
+      city,
+      state,
+      postal_code,
+      phone,
+      longitude,
+      latitude,
+    } = brewery;
+    let formattedPhone = phone
+      ? `${phone.slice(0, 3)}-${phone.slice(3, 6)}-${phone.slice(6)}`
+      : "No phone number available";
+    let info = document.createElement("div");
+    info.className = "brewery";
+    let breweryName = document.createElement("h3");
+    breweryName.style.fontWeight = "bold";
+    breweryName.textContent = name;
+
+    let breweryType = document.createElement("p");
+    breweryType.textContent = `Brewery type: ${brewery_type}`;
+
+    let phoneNum = document.createElement("a");
+    let phoneText = document.createElement("p");
+    phoneText.innerHTML = formattedPhone;
+
+    if (phone) {
+      phoneNum.href = `tel:${phone}`;
     }
-    catch(err){
-        brewResults.innerHTML = err.message
-        brewSearch.value = ""
-    }
-}
+    phoneNum.appendChild(phoneText);
 
-function listBreweries([{
-        name,
-        brewery_type,
-        street,
-        city,
-        state,
-        postal_code,
-        phone,
-        longitude,
-        latitude
-}]) {
-    brewSearch.value = ""
-    brewResults.innerHTML = `<h3>${name}</h3>
-    <h4>Brewery Type: ${brewery_type}</h4>
-    <p>Address:</p>
-    <p>${street}</p>
-    <p>${city}, ${state} ${postal_code}</p>
-    <p>Phone: ${phone}</p>    
-    `
-}
+    let mapLink = document.createElement("a");
+    let mapLinkText = document.createElement("p");
+    mapLink.href = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+    mapLink.target = "_blank";
+    mapLinkText.innerHTML = `${street} ${city}, ${state} ${postal_code}`;
+    mapLink.appendChild(mapLinkText);
+
+    info.replaceChildren(breweryName, breweryType, phoneNum, mapLink);
+    container.appendChild(info);
+  });
+  brewResults.replaceChildren(container);
+};
